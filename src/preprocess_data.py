15e3d5691214b7d2b05e7b5bbdaf30e8ee0769ab
Aledve <jakile971012@gmail.com>
2025-06-24 22:58:27 -0500

    Actualización de preprocess_data.py

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -2,7 +2,7 @@ import splitfolders
 
 # Divide los datos originales en 70% train, 15% val y 15% test
 splitfolders.ratio(
-    "data_orig",  # Carpeta de origen con las imágenes organizadas por clase
+    "data",  # Carpeta de origen con las imágenes organizadas por clase
     output="data",  # Carpeta de salida donde se generarán las subcarpetas train, val y test
     seed=42,  # Semilla para garantizar que la división sea reproducible
     ratio=(0.7, 0.15, 0.15),  # Proporción de los datos
